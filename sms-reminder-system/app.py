7843fa8f9513f67c6192c049b7703979aa5de5c3
lamprouChr <58117731+lamprouChr@users.noreply.github.com>
2025-05-28 17:42:41 +0300

    Update app.py

--- sms-reminder-system/app.py
+++ sms-reminder-system/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template, redirect, url_for
+
 from datetime import datetime
 from models import db, Appointment
 from dotenv import load_dotenv
@@ -42,35 +43,20 @@ def home():
         appointment_time = datetime.strptime(request.form["appointment"], "%Y-%m-%dT%H:%M")
         db.session.add(Appointment(name=name, phone=phone, appointment_time=appointment_time))
         db.session.commit()
-        return "Appointment added!"
+        return redirect(url_for("home"))
 
-
-
-#$gsdgsdgsd
-#fasfasfsa
-
-    # Fetch all appointments from DB
     appointments = Appointment.query.all()
+    return render_template("add_appointment.html", appointments=appointments)
 
-    # Render form AND appointment list
-    return render_template_string("""
-    <h2>Add Appointment</h2>
-    <form method="POST">
-      Name: <input name="name"><br>
-      Phone: <input name="phone"><br>
-      Appointment: <input type="datetime-local" name="appointment"><br>
-      <button type="submit">Add</button>
-    </form>
-    <hr>
-    <h3>Saved Appointments:</h3>
-    <ul>
-      {% for a in appointments %}
-        <li>{{ a.name }} - {{ a.phone }} - {{ a.appointment_time }}</li>
-      {% else %}
-        <li>No appointments yet.</li>
-      {% endfor %}
-    </ul>
-    """, appointments=appointments)
+
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete_appointment(id):
+    appointment = Appointment.query.get_or_404(id)
+    db.session.delete(appointment)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
